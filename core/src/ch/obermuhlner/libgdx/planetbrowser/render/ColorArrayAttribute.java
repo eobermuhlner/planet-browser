83b127bb0f46a0f6a492ef8d548c53d1e6b790f7
Eric Obermühlner <eobermuhlner@gmail.com>
2015-12-14 07:30:06 +0100

    javadoc to specify color array usage in shaders

--- core/src/ch/obermuhlner/libgdx/planetbrowser/render/ColorArrayAttribute.java
+++ core/src/ch/obermuhlner/libgdx/planetbrowser/render/ColorArrayAttribute.java
@@ -3,6 +3,25 @@ package ch.obermuhlner.libgdx.planetbrowser.render;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g3d.Attribute;
 
+/**
+ * Used to specify an array of colors for the shader to use.
+ * 
+ * How the shader uses the colors depends on the shader implementation.
+ * 
+ * <h2>jupiter shader</h2>
+ * See the file: <code>jupiter.fragment.glsl</code>
+ * 
+ * The first 3 colors define the color space used for the bands.
+ * Alpha values are ignored.
+ * 
+ * <h2>terrestrial shader</h2>
+ * See the file: <code>terrestrial.fragment.glsl</code>
+ * 
+ * The first 3 colors define the color space used for the minimum height.
+ * The first 3 colors define the color space used for the maximum height.
+ * Heights between minimum and maximum are interpolated.
+ * Alpha values are used to specify the specularity!
+ */
 public class ColorArrayAttribute extends Attribute {
 
 	public final static String PlanetColorsAlias = "planetColors";
