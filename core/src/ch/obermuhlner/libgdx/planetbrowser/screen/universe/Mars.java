991c6300376df143272db040acf5f3ed698a9bd8
Eric Obermühlner <eobermuhlner@gmail.com>
2015-12-26 20:07:49 +0100

    add some random molecules to mars atmosphere

--- core/src/ch/obermuhlner/libgdx/planetbrowser/screen/universe/Mars.java
+++ core/src/ch/obermuhlner/libgdx/planetbrowser/screen/universe/Mars.java
@@ -21,14 +21,28 @@ public class Mars extends AbstractRockyPlanet {
 					p(random.nextGaussian(3), Molecule.N2),
 					p(random.nextGaussian(0.001), Molecule.Ar),
 					p(random.nextGaussian(0.001), Molecule.O2),
-					p(random.nextGaussian(0.0001), Molecule.SO2)
+					p(random.nextGaussian(0.0001), Molecule.SO2),
+					// undocumented molecules - just for fun
+					p(random.nextBoolean(0.1) ? random.nextGaussian(0.001) : 0.0, Molecule.H2S),
+					p(random.nextBoolean(0.1) ? random.nextGaussian(0.001) : 0.0, Molecule.HCl),
+					p(random.nextBoolean(0.1) ? random.nextGaussian(0.001) : 0.0, Molecule.CH4),
+					p(random.nextBoolean(0.1) ? random.nextGaussian(0.001) : 0.0, Molecule.H2O),
+					p(random.nextBoolean(0.1) ? random.nextGaussian(0.0001) : 0.0, Molecule.Kr),
+					p(random.nextBoolean(0.1) ? random.nextGaussian(0.0001) : 0.0, Molecule.Ne)
 					);
 		} else {
 			// titan-line atmosphere
 			planetData.atmosphere = random.nextProbabilityMap(
 					p(random.nextGaussian(98), Molecule.N2),
 					p(random.nextGaussian(1), Molecule.CH4),
-					p(random.nextGaussian(0.5), Molecule.H2)
+					p(random.nextGaussian(0.5), Molecule.H2),
+					// undocumented molecules - just for fun
+					p(random.nextBoolean(0.1) ? random.nextGaussian(0.001) : 0.0, Molecule.H2S),
+					p(random.nextBoolean(0.1) ? random.nextGaussian(0.001) : 0.0, Molecule.HCl),
+					p(random.nextBoolean(0.1) ? random.nextGaussian(0.001) : 0.0, Molecule.CH4),
+					p(random.nextBoolean(0.1) ? random.nextGaussian(0.001) : 0.0, Molecule.H2O),
+					p(random.nextBoolean(0.1) ? random.nextGaussian(0.0001) : 0.0, Molecule.Kr),
+					p(random.nextBoolean(0.1) ? random.nextGaussian(0.0001) : 0.0, Molecule.Ne)
 					);
 		}
 		
