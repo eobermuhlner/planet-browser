8b6371b3f9ffe3bc371bfcc6e5d4ce0e373b4d6c
Eric Obermühlner <eobermuhlner@gmail.com>
2015-12-27 18:17:44 +0100

    add htmlLabel() supporting <sub> and <sup>

--- core/src/ch/obermuhlner/libgdx/planetbrowser/ui/Gui.java
+++ core/src/ch/obermuhlner/libgdx/planetbrowser/ui/Gui.java
@@ -1,5 +1,8 @@
 package ch.obermuhlner.libgdx.planetbrowser.ui;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -102,6 +105,54 @@ public class Gui {
 		SelectBox<String> selectBox = new SelectBox<String>(skin);
 		selectBox.setItems(items);
 		return selectBox;
+	}
+	
+	private static Pattern TAG_PATTERN = Pattern.compile("<(/?)([a-z]*)>");
+	public Table htmlLabel(String htmlText) {
+		Table table = new Table(skin);
+		
+		int sub = 0;
+		int sup = 0;
+		
+		Matcher matcher = TAG_PATTERN.matcher(htmlText);
+		int pos = 0;
+		while (matcher.find()) {
+			String text = htmlText.substring(pos, matcher.start());
+			addRichLabel(table, text, sub > 0, sup > 0);
+			
+			String tag = matcher.group(2);
+			boolean tagStart = matcher.group(1).length() == 0;
+			
+			if (tag.equals("sub")) {
+				sub += tagStart ? 1 : -1;
+			} else if (tag.equals("sup")) {
+				sup += tagStart ? 1 : -1;
+			}
+			
+			pos = matcher.end();
+		}
+		
+		String text = htmlText.substring(pos);
+		addRichLabel(table, text, sub > 0, sup > 0);
+		return table;
+	}
+
+	private void addRichLabel(Table table, String text, boolean sub, boolean sup) {
+		if (sub) {
+			table.add(text, "small").bottom().spaceLeft(1).spaceRight(2);
+		} else if (sup) {
+			table.add(text, "small").top().spaceLeft(1).spaceRight(2);
+		} else {
+			table.add(text);
+		}
+	}
+	
+	public static String firstToUppercase(String string) {
+		if (string.length() == 0) {
+			return "";
+		}
+		
+		return string.substring(0, 1).toUpperCase() + string.substring(1);
 	}
 
 	public class TableLayout extends Table {
