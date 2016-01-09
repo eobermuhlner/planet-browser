cdc274b9e8c96504d54ef776bdf6895c2cb3d9d0
Eric Obermühlner <eobermuhlner@gmail.com>
2016-01-09 18:10:23 +0100

    added Terrain with moving TerrainChunks

--- core/src/ch/obermuhlner/libgdx/planetbrowser/screen/FlyPlanetScreen.java
+++ core/src/ch/obermuhlner/libgdx/planetbrowser/screen/FlyPlanetScreen.java
@@ -21,8 +21,10 @@ import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
 
 import ch.obermuhlner.libgdx.planetbrowser.PlanetBrowser;
@@ -33,22 +35,32 @@ import ch.obermuhlner.libgdx.planetbrowser.render.PlanetUberShaderProvider;
 import ch.obermuhlner.libgdx.planetbrowser.screen.universe.ModelInstanceFactory;
 import ch.obermuhlner.libgdx.planetbrowser.screen.universe.PlanetData;
 import ch.obermuhlner.libgdx.planetbrowser.ui.Gui;
+import ch.obermuhlner.libgdx.planetbrowser.ui.Gui.TableLayout;
 import ch.obermuhlner.libgdx.planetbrowser.util.Random;
+import ch.obermuhlner.libgdx.planetbrowser.util.StopWatch;
 
 public class FlyPlanetScreen extends AbstractScreen {
+
+	private static final boolean SHOW_DEBUG_INFO = true;
 
 	private final ModelInstanceFactory factory;
 	private final long randomSeed;
 
-	public final Environment environment = new Environment();
+	private final Environment environment = new Environment();
 	private Stage stage;
 	private CameraInputController cameraInputController;
 	private ModelBatch modelBatch;
 	private PerspectiveCamera camera;
+	
+	private Label fpsLabel;
+	private Label deltaMillisLabel;
+	private Label createTimeLabel;
+
 	private PlanetData planetData;
 	private Color atmosphereColor;
 
-	private TerrainChunk[] terrain = new TerrainChunk[9];
+	private Terrain terrain = new Terrain();
+	//private TerrainChunk[] terrain = new TerrainChunk[9];
 	
 	public FlyPlanetScreen(ModelInstanceFactory factory, long randomSeed) {
 		this.factory = factory;
@@ -94,48 +106,14 @@ public class FlyPlanetScreen extends AbstractScreen {
 
 		prepareStage();
 
-		//int textureSize = PlanetBrowser.INSTANCE.options.getGeneratedTexturesSize();
-		int textureSize = 64;
-		float terrainSize = 5.0f;
-		int meshDivisions = 8;
-		float stepX = 0.1f;
-		float stepY = 0.1f;
-		for (int dy = 0; dy < 3; dy++) {
-			for (int dx = 0; dx < 3; dx++) {
-				boolean best = dx == 0 && dy == 0;
-				float fromX = 0.5f - stepX * dx;
-				float fromY = 0.5f + stepY * dy;
-				float terrainX = dx * terrainSize * 2;
-				float terrainY = dy * terrainSize * 2;
-				terrain[dy * 3 + dx] = createTerrainChunk(
-						fromX, fromX + stepX, fromY, fromY + stepY, 
-						best ? 1024 : textureSize, 
-						best ? 32 : meshDivisions, 
-						terrainX, terrainY, terrainSize);
-			}
-		}
+		terrain.planetX = 0.5f;
+		terrain.planetY = 0.5f;
+		terrain.planetStep = 0.1f;
+		terrain.terrainX = 0f;
+		terrain.terrainY = 0f;
+		terrain.terrainStep = 5f;
 	}
 	
-	private TerrainChunk createTerrainChunk(float xFrom, float xTo, float yFrom, float yTo, int textureSize, int meshDivisions, float terrainX, float terrainY, float terrainSize) {
-		long textureTypes = TextureAttribute.Diffuse | TextureAttribute.Normal | TextureAttribute.Specular;
-		Map<Long, Texture> textures = factory.createTextures(planetData, new Random(randomSeed), xFrom, xTo, yFrom, yTo, textureTypes, textureSize);
-
-		Array<Attribute> materialAttributes = new Array<Attribute>();
-		materialAttributes.add(new TextureAttribute(TextureAttribute.Diffuse, textures.get(TextureAttribute.Diffuse)));
-		materialAttributes.add(new TextureAttribute(TextureAttribute.Normal, textures.get(TextureAttribute.Normal)));
-		materialAttributes.add(new TextureAttribute(TextureAttribute.Specular, textures.get(TextureAttribute.Specular)));
-
-		Texture bumpTexture = factory.createTextures(planetData, new Random(randomSeed), xFrom, xTo, yFrom, yTo, TextureAttribute.Bump, meshDivisions).get(TextureAttribute.Bump);
-		materialAttributes.add(new TextureAttribute(TextureAttribute.Bump, bumpTexture));
-		Material material = new Material(materialAttributes);
-		
-		ModelInstance modelInstance = createTerrainMesh(bumpTexture, terrainSize, material, 0, 1, 0, 1);
-		TerrainChunk chunk = new TerrainChunk();
-		chunk.surface = modelInstance;
-		chunk.surface.transform.setToTranslation(terrainX, 0, terrainY);
-		return chunk;
-	}
-
 	private void prepareStage() {
 		Gui gui = new Gui();
 		Table rootTable = gui.rootTable();
@@ -156,13 +134,44 @@ public class FlyPlanetScreen extends AbstractScreen {
 				}
 			}));
 		}
+
+		{
+			// dummy to have the center empty
+			rootTable.row().expandY();
+			rootTable.add("");
+		}
+
+		{
+			rootTable.row();
+			Table infoPanel = gui.table();
+			rootTable.add(infoPanel).align(Align.bottomLeft);
+			if (SHOW_DEBUG_INFO) {
+				TableLayout tableLayout = gui.tableLayout();
+				infoPanel.row();
+				infoPanel.add("FPS");
+				infoPanel.add(tableLayout.right());
+		
+				fpsLabel = tableLayout.addNumeric("888");
+				tableLayout.add(" (");
+				deltaMillisLabel = tableLayout.addNumeric("8888");
+				tableLayout.add(" ms)");
+			}
+			
+			if (SHOW_DEBUG_INFO) {
+				infoPanel.row();
+				infoPanel.add("Planet Creation");
+				createTimeLabel = gui.label("");
+				infoPanel.add(createTimeLabel);
+			}
+		}
 	}
+	
 	private ModelInstance createTerrainMesh(Texture bumpTexture, float rectSize, Material material, float uFrom, float uTo, float vFrom, float vTo) {
 		final VertexInfo vertTmp1 = new VertexInfo();
 
 		ModelBuilder modelBuilder = new ModelBuilder();
 		modelBuilder.begin();
-		MeshPartBuilder part = modelBuilder.part("terrain", GL20.GL_TRIANGLES, (long) (Usage.Position | Usage.Normal | Usage.Tangent | Usage.TextureCoordinates), material);
+		MeshPartBuilder part = modelBuilder.part("terrain", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal | Usage.Tangent | Usage.TextureCoordinates, material);
 
 		int divisionsU = bumpTexture.getWidth();
 		int divisionsV = bumpTexture.getHeight();
@@ -229,20 +238,136 @@ public class FlyPlanetScreen extends AbstractScreen {
 		stage.act(delta);
 		cameraInputController.update();
 
+		
 		modelBatch.begin(camera);
-		for (int i = 0; i < terrain.length; i++) {
-			modelBatch.render(terrain[i].surface, environment);
-		}
+		terrain.center(camera.position.x, camera.position.z);
+		terrain.render(modelBatch, environment);
 		modelBatch.end();
+		
+		fpsLabel.setText(String.valueOf(Gdx.graphics.getFramesPerSecond()));
+		deltaMillisLabel.setText(String.valueOf((int) (Gdx.graphics.getDeltaTime() * 1000)));
+
 		stage.draw();
+	}
+
+	private class Terrain {
+		private int chunkCount = 3; // number of chunks in one axis
+		private float planetX;
+		private float planetY;
+		private float planetStep;
+		
+		private float terrainX;
+		private float terrainY;
+		private float terrainStep;
+
+		private int textureSize = 1024;
+		private int meshDivisions = 128;
+		
+		private TerrainChunk[] terrain = new TerrainChunk[chunkCount * chunkCount];
+		private TerrainChunk[] terrainCopy = new TerrainChunk[chunkCount * chunkCount];
+
+		public Terrain() {
+			for (int i = 0; i < terrain.length; i++) {
+				terrain[i] = new TerrainChunk();
+			}
+		}
+		
+		public void center(float cameraTerrainX, float cameraTerrainY) {
+			int x = (int) ((cameraTerrainX - terrainX - terrainStep / 2) / terrainStep);
+			int y = (int) ((cameraTerrainY - terrainY - terrainStep / 2) / terrainStep);
+			//System.out.println("CENTER " + x + " " + y);
+			if (x != 0 || y != 0) {
+				System.out.println("MOVE " + x + " " + y);
+				moveChunks(x, y);
+			}
+		}
+
+		private void moveChunks(int moveX, int moveY) {
+			for (int i = 0; i < terrain.length; i++) {
+				int chunkX = i % chunkCount;
+				int chunkY = i / chunkCount;
+				
+				int moveChunkX = chunkX + moveX;
+				int moveChunkY = chunkY + moveY;
+				if (moveChunkX >= 0 && moveChunkX < chunkCount && moveChunkY >= 0 && moveChunkY < chunkCount) {
+					int moveIndex = moveChunkX + moveChunkY * chunkCount;
+					terrainCopy[i] = terrain[moveIndex];
+				} else {
+					terrainCopy[i] = new TerrainChunk();
+				}
+			}
+			TerrainChunk[] tmp = terrain; 
+			terrain = terrainCopy;
+			terrainCopy = tmp;
+			
+			terrainX += moveX * terrainStep;
+			terrainY += moveY * terrainStep;
+			planetX += moveX * planetStep;
+			planetY += moveY * planetStep;
+		}
+
+		public void render(ModelBatch modelBatch, Environment environment) {
+			StringBuilder createTimeText = null;
+			for (int i = 0; i < terrain.length; i++) {
+				int chunkX = i % chunkCount;
+				int chunkY = i / chunkCount;
+
+				if (terrain[i].surface == null) {
+					if (createTimeText == null) {
+						createTimeText = new StringBuilder();
+					} else {
+						createTimeText.append(" ");
+					}
+					StopWatch stopWatch = new StopWatch();
+					terrain[i].surface = createTerrainSurface(chunkX, chunkY);
+					createTimeText.append((int) stopWatch.getElapsedMilliseconds());
+				}
+				
+				int centerChunk = chunkCount / 2;
+				float chunkTerrainX = terrainX + (chunkX - centerChunk) * terrainStep * 2;
+				float chunkTerrainY = terrainY + (chunkY - centerChunk) * terrainStep * 2;
+				terrain[i].surface.transform.setTranslation(chunkTerrainX, 0, chunkTerrainY);
+				
+				modelBatch.render(terrain[i].surface, environment);
+			}
+			if (createTimeText != null) {
+				createTimeText.append(" ms");
+				createTimeLabel.setText(createTimeText.toString());
+			}
+		}
+
+		private ModelInstance createTerrainSurface(int dx, int dy) {
+			float fromX = planetX - planetStep * dx;
+			float fromY = planetY + planetStep * dy;
+			return createTerrainSurface(
+					fromX, fromX + planetStep, fromY, fromY + planetStep, 
+					textureSize, 
+					meshDivisions, 
+					terrainStep);
+		}
+		
+		private ModelInstance createTerrainSurface(float xFrom, float xTo, float yFrom, float yTo, int textureSize, int meshDivisions, float terrainSize) {
+			StopWatch stopWatch = new StopWatch();
+			long textureTypes = TextureAttribute.Diffuse | TextureAttribute.Normal | TextureAttribute.Specular;
+			Map<Long, Texture> textures = factory.createTextures(planetData, new Random(randomSeed), xFrom, xTo, yFrom, yTo, textureTypes, textureSize);
+			System.out.println("create main textures " + stopWatch);
+			Array<Attribute> materialAttributes = new Array<Attribute>();
+			materialAttributes.add(new TextureAttribute(TextureAttribute.Diffuse, textures.get(TextureAttribute.Diffuse)));
+			materialAttributes.add(new TextureAttribute(TextureAttribute.Normal, textures.get(TextureAttribute.Normal)));
+			materialAttributes.add(new TextureAttribute(TextureAttribute.Specular, textures.get(TextureAttribute.Specular)));
+
+			stopWatch.start();
+			Texture bumpTexture = factory.createTextures(planetData, new Random(randomSeed), xFrom, xTo, yFrom, yTo, TextureAttribute.Bump, meshDivisions).get(TextureAttribute.Bump);
+			System.out.println("create bump texture " + stopWatch);
+			materialAttributes.add(new TextureAttribute(TextureAttribute.Bump, bumpTexture));
+			Material material = new Material(materialAttributes);
+			
+			ModelInstance modelInstance = createTerrainMesh(bumpTexture, terrainSize, material, 0, 1, 0, 1);
+			return modelInstance;
+		}
 	}
 	
 	private static class TerrainChunk {
-		float xFrom;
-		float xTo;
-		float yFrom;
-		float yTo;
-		
 		ModelInstance surface;
 	}
 }
