ea696335cd2475ece77752763f464990476f2984
Eric Obermühlner <eobermuhlner@gmail.com>
2016-01-14 21:48:24 +0100

    faster ship

--- core/src/ch/obermuhlner/libgdx/planetbrowser/control/Ship.java
+++ core/src/ch/obermuhlner/libgdx/planetbrowser/control/Ship.java
@@ -1,9 +1,9 @@
 package ch.obermuhlner.libgdx.planetbrowser.control;
 
 public class Ship {
-	public float forwardThrust = 5000000f;
-	public float upThrust = 10000f;
-	public float rightThrust = 10000f;
+	public float forwardThrust = 10000000f;
+	public float upThrust = 100000f;
+	public float rightThrust = 100000f;
 	public float rollThrust = 1f;
 	public float pitchThrust = 1f;
 	public float yawThrust = 1f;
